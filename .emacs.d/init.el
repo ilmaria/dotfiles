d57183ee9297cf4f29beed04231ff48faaef6198
Ilmari Autio <ilmari.autio@gmail.com>
2017-12-06 16:09:38 +0200

    Add emacs theme

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -41,17 +41,6 @@
 
 (custom-set-variables
  '(inhibit-startup-screen t)
- '(package-selected-packages (quote (dumb-jump))))
+ '(package-selected-packages (quote ((gruvbox-them dumb-jump))))
 
-;; Syntax highlighting
-(custom-set-faces
-  '(default ((t (:background "#fdf6e3" :foreground "#586e75"))))
-  '(font-lock-builtin-face ((t (:foreground "inherit"))))
-  '(font-lock-comment-face ((t (:foreground "#b58900"))))
-  '(font-lock-constant-face ((t (:foreground "inherit"))))
-  '(font-lock-doc-string-face ((t (:foreground "inherit"))))
-  '(font-lock-function-name-face ((t (:foreground "inherit"))))
-  '(font-lock-keyword-face ((t (:foreground "inherit"))))
-  '(font-lock-preprocessor-face ((t (:foreground "inherit"))))
-  '(font-lock-reference-face ((t (:foreground "inherit"))))
-  '(font-lock-string-face ((t (:foreground "inherit")))))
+(load-theme 'gruvbox-dark-medium t)

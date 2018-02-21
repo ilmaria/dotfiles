497a020542727dda50da4d28344da126546e35fc
Ilmari Autio <ilmari.autio@gmail.com>
2018-02-21 11:23:52 +0200

    Workaround didn't work

--- nixos-config/desktop.nix
+++ nixos-config/desktop.nix
@@ -6,10 +6,4 @@
     device = "/dev/disk/by-uuid/d49f25dd-cb21-4cca-aa93-8dd38b0d91e5";
     fsType = "ext4";
   };
-
-  
-  services.xserver.displayManager.sessionCommands = lib.mkForce ''
-    # Workaround to enable elvish command history
-    rm -rf /tmp/elvish*
-  '';
 }

854bcd94cf8157c95c1386c446a6a9500fb8adf0
Ilmari Autio <ilmari.autio@gmail.com>
2018-04-17 16:20:21 +0300

    Simplify xmonad config with one fullscreen layout

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -2,16 +2,15 @@
 import XMonad.Config.Prime
 
 import qualified XMonad as XM
-import XMonad (XConfig, xK_e, sendMessage)
+import XMonad (XConfig, sendMessage)
 import XMonad.Actions.CycleWS (prevWS, nextWS)
-import XMonad.Actions.SwapWorkspaces (swapWithCurrent)
 import XMonad.Actions.WindowNavigation (withWindowNavigation)
-import XMonad.Hooks.DynamicLog (statusBar, xmobarPP, xmobarColor, wrap, ppCurrent)
-import XMonad.Hooks.ManageDocks (ToggleStruts(..))
+import XMonad.Hooks.DynamicLog (xmobar)
+import XMonad.Hooks.ManageDocks (avoidStruts)
 import XMonad.Layout.Fullscreen (fullscreenSupport)
 import XMonad.Layout.NoBorders (smartBorders)
 import XMonad.Layout.Tabbed (simpleTabbed)
-import XMonad.Layout.Spacing (spacing, spacingWithEdge, setSpacing, incSpacing)
+import XMonad.Layout.Spacing (spacing, spacingWithEdge, incSpacing)
 
 mySpacing = 15
 
@@ -22,11 +21,11 @@ main = xmonad $ do
     terminal =: "urxvt"
 --  focusFollowsMouse =: False
     clickJustFocuses =: False
-    resetLayout $ spacingWithEdge mySpacing $ Tall 1 (3/100) (1/2) ||| Full
+    applyIO $ xmobar
+    resetLayout $ avoidStruts (spacingWithEdge mySpacing (Tall 1 (3/100) (1/2))) ||| Full
     modifyLayout smartBorders
     apply fullscreenSupport
     applyIO $ withWindowNavigation (xK_w, xK_a, xK_s, xK_d)
-    applyIO $ statusBar "xmobar" statusPP toggleStatusKey
     keys =+ [
           ("M-2",                    spawn "firefox")
         , ("M-3",                    spawn "urxvt")
@@ -36,17 +35,6 @@ main = xmonad $ do
         , ("<XF86AudioLowerVolume>", spawn "amixer sset Master 3%-")
         , ("M-<Right>",              nextWS)
         , ("M-<Left>",               prevWS)
-        , ("M-n",                    sendMessage ToggleStruts)
-        , ("M-i",                    setSpacing 0)
-        , ("M-o",                    setSpacing mySpacing)
         , ("M-<KP_Add>",             incSpacing (1))
         , ("M-<KP_Subtract>",        incSpacing (-1))
         ]
-
-statusPP = xmobarPP {
-    ppCurrent = xmobarColor "#429942" "" . wrap "<" ">"
-}
-
-toggleStatusKey XConfig {XM.modMask = modMaskKey} =
-    (modMaskKey, xK_e)
-

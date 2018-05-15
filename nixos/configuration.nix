5fd3be893523ada77be16948f9d7719fede51b56
Ilmari Autio <ilmari.autio@gmail.com>
2018-05-15 21:29:22 +0300

    Use grub for booting

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -5,10 +5,6 @@ let
     config.allowUnfree = true;
   };
 in {
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.grub.useOSProber = true;
 
   boot.cleanTmpDir = true;
 
--- nixos/desktop.nix
+++ nixos/desktop.nix
@@ -6,4 +6,22 @@
     device = "/dev/disk/by-uuid/d49f25dd-cb21-4cca-aa93-8dd38b0d91e5";
     fsType = "ext4";
   };
+
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.loader.grub.devices = [ "nodev" ];
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  boot.loader.grub.extraEntries = ''
+    menuentry "Windows" {
+      insmod part_gpt
+      insmod fat
+      insmod search_fs_uuid
+      insmod chain
+      search --fs--uid --set=root fe505ac3-54aa-4605-9baa-40bb62f0d7ec
+      chainloader /EFI/Microsoft/Boot/bootmgfw.efi
+    }
+  '';
+
 }
